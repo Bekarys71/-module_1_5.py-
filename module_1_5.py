240394cc080eb073d260758d8a000354cdecc16d
Bekarys71 <Beybarys.imashev@mail.ru>
2024-09-08 15:32:03 +0600

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = "tuple_"
+tuple_= 1 , 2,3 , "hello world"
+print(tuple_ )
+print(tuple_ [3])
+#tuple_[3]= 6 #потому что кортежи  неизменяемый тип
+mutable_list = [ 1 ,2,3, "hello world"]
+print(mutable_list)
+mutable_list[3]= "Меня зовут бекарыс"
+print(mutable_list)
